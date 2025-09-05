b19148d1a01d59afd76d184cf8a4af9590c2599b
Marcus Vinícius <mvini.maciell@gmail.com>
2025-09-05 14:07:06 -0300

    Criando projeto

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["zmvov/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zmvov/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
